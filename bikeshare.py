0e26d31f66c50bd489c2582f8533619a1c36e44f
Najla-Ibrahim-Alkhaldi <najlalkhaldi99@gmail.com>
2023-02-13 22:46:23 +0300

    Make the input message shorter

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = input('would you like to see data for Chicago, New York, or Washington?').lower()
+    city = input('would you like to see data for Chicago, NYC, or Washington?').lower()
     while city not in CITY_DATA.keys():
             city = input('Please inter a valid name. Chicago, New York, or Washington?  ').lower()
 
